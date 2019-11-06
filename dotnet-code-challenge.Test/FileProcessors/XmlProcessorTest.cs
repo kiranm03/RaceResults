6439a1b3b55bae3767b61e61c60a13bf847aca3e
Kiran Mandalapu <kiran@192-168-1-102.tpgi.com.au>
2019-11-07 09:59:59 +1100

    Unit tests for Xml processor

---
+++ dotnet-code-challenge.Test/FileProcessors/XmlProcessorTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using dotnet_code_challenge.FileProcessors;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace dotnet_code_challenge.Test.FileProcessors
+{
+    [TestClass]
+    public class XmlProcessorTest
+    {
+        private XmlProcessor _subject;
+        private readonly Mock<ILogger<XmlProcessor>> _logger = new Mock<ILogger<XmlProcessor>>();
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _subject = new XmlProcessor(_logger.Object);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Process_EmptyPath_ThrowsArgumentException()
+        {
+            //Arrange
+            var filePath = string.Empty;
+            //Act
+            var actual = _subject.Process(filePath);
+            //Assert
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void Process_FileNotFoundInPath_ThrowsFileNotFoundException()
+        {
+            //Arrange
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory);
+            //Act
+            var actual = _subject.Process(filePath);
+            //Assert
+        }
+    }
+}
--- dotnet-code-challenge/FileProcessors/XmlProcessor.cs
+++ dotnet-code-challenge/FileProcessors/XmlProcessor.cs
@@ -1,20 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using dotnet_code_challenge.Models;
+using Microsoft.Extensions.Logging;
 
 namespace dotnet_code_challenge.FileProcessors
 {
     public class XmlProcessor : ProcessorStrategy
     {
-        public XmlProcessor()
+        private readonly ILogger<XmlProcessor> _logger;
+
+        public XmlProcessor(ILogger<XmlProcessor> logger)
         {
+            _logger = logger;
         }
 
-        public override FileProcessorType ProcessorType => throw new NotImplementedException();
-
+        public override FileProcessorType ProcessorType
+        {
+            get
+            {
+                return FileProcessorType.XmlProcessor;
+            }
+        }
         public override IEnumerable<Horse> Process(string filePath)
         {
-            throw new NotImplementedException();
+            ValidateFilePath(filePath);
+            return null;
         }
     }
 }
